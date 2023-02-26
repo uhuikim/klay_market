d16beb535752368a0819e5d05ad7d3b1d281dac7
blockodyssey-uhui <reah@blockodyssey.io>
2023-02-26 21:57:23 +0900

    style : 글자 ...처리

--- src/pages/my/index.jsx
+++ src/pages/my/index.jsx
@@ -37,8 +37,17 @@ const My = () => {
 
     return (
         <div>
-            <p>주소 : {address}</p>
-            <p>잔액 : {balance}</p>
+            <div css={infoWrap}>
+                <p>
+                    <span css={infoTitle}>주소</span>
+                    {address}
+                </p>
+                <p>
+                    <span css={infoTitle}>잔액</span>
+                    {balance} KLAY
+                </p>
+            </div>
+
             <div css={container}>
                 {myNfts.map((nft) => (
                     <Card
@@ -67,4 +76,28 @@ const container = css`
     @media (min-width: 760px) {
         grid-template-columns: repeat(4, minmax(0, 1fr));
     }
+`;
+
+const infoWrap = css`
+    border-radius: 10px;
+    background: #eceff1;
+    padding: 1.5rem 2rem;
+    box-shadow: rgb(21 22 25 / 5%) 0px 4px 8px;
+    margin-bottom: 1rem;
+
+    > p {
+        word-break: break-all;
+        white-space: pre;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    & > p:first-child {
+        margin-bottom: 0.5rem;
+    }
+`;
+
+const infoTitle = css`
+    margin-right: 1rem;
+    font-weight: bold;
 `;
